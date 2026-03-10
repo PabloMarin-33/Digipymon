724ff4eee84aae7eaa6b00a198e2e20f5feaeeec
Palomo-33 <pablomarinortega37@gmail.com>
2026-03-10 12:51:13 +0100

    inicio del main

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+def generar_digipymon_aleatorio():
+
+    tipos = ["agua", "fuego", "planta"]
+
+    vida = random.randint(10, 20)
+    ataque = random.randint(1, 10)
+    nivel = random.randint(1, 3)
+    tipo = random.choice(tipos)
+    nombre = listanombres
+
