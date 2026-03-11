e4a56d22902796ff3c3a9cbe796c7fddd25b7fb4
Palomo-33 <pablomarinortega37@gmail.com>
2026-03-11 12:56:41 +0100

    funcion de generar digipymon hecha

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import random
+import listanombres
+import digipymon
+
 
 def generar_digipymon_aleatorio():
-
     tipos = ["agua", "fuego", "planta"]
+    nombres = listanombres.listanombres([], [])
 
     vida = random.randint(10, 20)
     ataque = random.randint(1, 10)
     nivel = random.randint(1, 3)
     tipo = random.choice(tipos)
-    nombre = listanombres
+    nombre = nombres.obtener_nombre_digipymon()
 
+    return digipymon.digipymon(nombre, vida, ataque, tipo, nivel)
+
+def menu():
+    
