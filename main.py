619895cc1d610a7ad279b990e75fd8f56eb4e705
Palomo-33 <pablomarinortega37@gmail.com>
2026-03-13 11:46:04 +0100

    funcion del menu y buscar digipymon hecha

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import listanombres
 import digipymon
+import jugador
 
 
 def generar_digipymon_aleatorio():
@@ -15,5 +16,47 @@ def generar_digipymon_aleatorio():
 
     return digipymon.digipymon(nombre, vida, ataque, tipo, nivel)
 
+
 def menu():
-    
+    print("===Menu===")
+    print("===1. Buscar Digipymon ===")
+    print("===2. Luchar con Entrenador ===")
+    print("===3. Ir a la Tienda ===")
+    print("===4. Usar Onjetos ===")
+    print("===5. Consultar Inventario ===")
+    print("===6. Consultar Digipymon ===")
+    print("===7. Salir ===")
+
+    return input(("Introduce tu opción: "))
+
+
+def buscar_digipymon(jugador, inventario):
+    digipymon_salvaje = generar_digipymon_aleatorio()
+    print(f"¡Has encontrado un {digipymon_salvaje.nombre}!")
+    print(digipymon_salvaje)
+
+    probabilidad_captura = 100 - (digipymon_salvaje.nivel * 10)
+    print(f"Probabilidad de captura: {probabilidad_captura}%")
+
+    decision = input("¿Quieres capturar el Digipymon? (s/n): ").lower()
+
+    if decision == "s":
+        if jugador.cantidad_digipymon >= 6:
+            print("¡No tienes espacio para más Digipymon!")
+            return
+        if (
+            "digipyballs" not in inventario.objetos
+            or inventario.objetos["digipyballs"] <= 0
+        ):
+            print("¡No tienes digipyballs!")
+            return
+
+        inventario.usar_objeto("digipyballs")
+        intento = random.randint(1, 100)
+        if intento <= probabilidad_captura:
+            jugador.añadir_digipymon(digipymon_salvaje)
+            print(f"¡Has capturado a {digipymon_salvaje.nombre}!")
+        else:
+            print(f"¡{digipymon_salvaje.nombre} se ha escapado!")
+    else:
+        print("Has huido del Digipymon.")
